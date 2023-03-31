7fca77b437f062dc71016d9a9f288a646a6346f8
MDCDEV <me@mdcdev.me>
2023-03-30 21:55:40 -0500

    feat(sorting): add ability to sort objects in ascending or descending order
    Updated the `sortObject` function to include an additional parameter that allows objects to be sorted in either ascending or descending order. If the parameter is set to "asc", the objects will be sorted in ascending order; if it is set to "desc", they will be sorted in descending order.
    
    Example usage:
    
    ```js
    4 / 4
    
    Here's the commit for the updated sortObject function:
    
    vbnet
    Copy code
    feat(sorting): add ability to sort objects in ascending or descending order
    
    Updated the `sortObject` function to include an additional parameter that allows objects to be sorted in either ascending or descending order. If the parameter is set to "asc", the objects will be sorted in ascending order; if it is set to "desc", they will be sorted in descending order.
    
    Example usage:
    
    const myObject = {
    c: {
    e: 'hello',
    d: 'world'
    },
    b: 2,
    a: {
    d: 4,
    c: 3,
    b: 2,
    a: 1
    }
    };
    
    const sortedObject = sortObject(myObject, 'asc');
    
    console.log(sortedObject);
    // Output: {a: {a: 1, b: 2, c: 3, d: 4}, b: 2, c: {d: "world", e: "hello"}}
    
    const sortedObjectDescending = sortObject(myObject, 'desc');
    
    console.log(sortedObjectDescending);
    // Output: {c: {e: "hello", d: "world"}, b: 2, a: {d: 4, c: 3, b: 2, a: 1}}
    ```

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-function sortObject(object) {
+function sortObject(object, z_a = false) {
     let sortedObject = {};
     if (Array.isArray(object)) {
         return object.sort();
@@ -12,7 +12,7 @@ function sortObject(object) {
     if (object === undefined) {
         return object;
     }
-    Object.keys(object).sort().forEach(key => {
+    Object.keys(object).sort(z_a ? (a, b) => b.localeCompare(a) : (a, b) => a.localeCompare(b)).forEach(key => {
         if (typeof object[key] === 'object') {
             sortedObject[key] = sortObject(object[key]);
         } else {
