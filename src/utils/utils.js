7ec71d1069b52bcc6f2f91bc30683b0460b9f7ad
MDCDEV <me@mdcdev.me>
2024-02-06 01:38:30 -0500

    refactor: Delete unnecessary check undefined

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,9 +9,6 @@ function sortObject(object, z_a = false) {
     if (object === null) {
         return object;
     }
-    if (object === undefined) {
-        return object;
-    }
     Object.keys(object).sort(z_a ? (a, b) => b.localeCompare(a) : (a, b) => a.localeCompare(b)).forEach(key => {
         if (typeof object[key] === 'object') {
             sortedObject[key] = sortObject(object[key]);
