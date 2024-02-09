d0203d2fc631025ea365a5b2b2b63488fa99472d
MDCDEV <me@mdcdev.me>
2024-02-09 18:37:54 -0500

    feat: New default font

--- src/routes/v2/avatar-generator/index.js
+++ src/routes/v2/avatar-generator/index.js
@@ -78,7 +78,7 @@ router.get('/:text', limit, async (req, res) => {
     if (!fontcolor || !hexColorRegex.test(fontcolor)) fontcolor = '#000000';
     else fontcolor = `#${fontcolor}`
 
-    if (!font || !GlobalFonts.has(font)) font = "arial"
+    if (!font || !GlobalFonts.has(font)) font = "roboto"
     font = font.toLowerCase();
 
     if (!backgroundcolor || !hexColorRegex.test(backgroundcolor)) backgroundcolor = colors[Math.round(Math.abs(text.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)) % 100) % colors.length];
@@ -91,7 +91,7 @@ router.get('/:text', limit, async (req, res) => {
     ctx.fillStyle = backgroundcolor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = fontcolor;
-    ctx.font = `${fontsize}px ${font}, arial2`;
+    ctx.font = `${fontsize}px ${font}, roboto`;
     ctx.fillText(text, canvas.width / 2 - ctx.measureText(text).width / 2, canvas.height / 2 + 12);
     // Save image to cache
     await cache.set(`${text} ${fontsize} ${size} ${fontcolor} ${backgroundcolor} ${font}`, await canvas.encode('png'));
