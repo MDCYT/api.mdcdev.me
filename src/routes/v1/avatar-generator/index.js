e42ab4333d239fd650f52916ff1117fad74cf922
MDCDEV <me@mdcdev.me>
2024-02-09 00:02:28 -0500

    feat: New options for avatar generation

--- src/routes/v1/avatar-generator/index.js
+++ src/routes/v1/avatar-generator/index.js
@@ -7,14 +7,18 @@ const RedisRateLimit = require(join(__basedir, 'utils', 'rate-limit'));
 const { statusCodeHandler } = require(join(__basedir, 'utils', 'status-code-handler'));
 const { Cache } = require(join(__basedir, 'utils', 'cache'));
 
-const cache = new Cache("avatar-generator", 1, 60 * 60 * 24)
-
 const { GlobalFonts, createCanvas } = require('@napi-rs/canvas');
 
-GlobalFonts.registerFromPath(join(__basedir, 'fonts', 'ARIAL.TTF'), 'arial');
+const fs = require('node:fs');
 
-const canvas = createCanvas(120, 120);
-const ctx = canvas.getContext('2d');
+fs.readdirSync(join(__basedir, 'fonts')).forEach(async file => {
+    if (file.endsWith(".ttf")) {
+        GlobalFonts.registerFromPath(join(__basedir, 'fonts', file), file.replace(".ttf", ""))
+    }
+});
+
+
+const cache = new Cache("avatar-generator", 1, 60 * 60 * 24 * 30)
 
 const colors = [
     "#FFD1DC", // Rosa pastel
@@ -22,19 +26,21 @@ const colors = [
     "#E6E6FA", // Lavanda claro
     "#FFFACD", // Amarillo crema
     "#98FB98"  // Verde menta suave
-  ];
+];
 
 const limit = rateLimit({
-    windowMs: 1000 * 60 * 60, // 1 hour window
+    windowMs: 1000 * 60 * 15, // 15 minutes window
     max: (req, res) => {
-        return 50;
-    }, // start blocking after 25 requests
+        return 200;
+    }, // start blocking after 200 requests
     message: (req, res) => {
         statusCodeHandler({ statusCode: 10001 }, res);
     },
     skip: (req, res) => {
-        //If the :id is process.env.OWNER_DISCORD_SERVER_INVITE, skip the rate limit
-        if (req.params.id === process.env.OWNER_DISCORD_SERVER_INVITE) return true;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        if (ip === 'localhost' || ip === '::1') {
+            return true;
+        }
         return false;
     },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
@@ -46,24 +52,52 @@ router.get('/:text', limit, async (req, res) => {
 
     const { text } = req.params;
 
+    let { fontsize, size, fontcolor, backgroundcolor, font } = req.query;
+
     // If the cache has the image, return it
-    if (await cache.has(text)) {
+    if (await cache.has(`${text} ${fontsize} ${size} ${fontcolor} ${backgroundcolor} ${font}`)) {
         res.setHeader('Content-Type', 'image/png');
         res.send(Buffer.from(await cache.get(text)));
         return;
     }
 
+
+    const hexColorRegex = /^(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/
+
+    fontsize = Number(fontsize)
+    size = Number(size)
+
+    if (!fontsize) fontsize = 48;
+    if (fontsize <= 0) size = 48;
+    if (fontsize >= 512) fontsize = 512;
+
+    if (!size) size = 120;
+    if (size <= 0) size = 120;
+    if (size >= 1024) size = 1024;
+
+    if (!fontcolor || !hexColorRegex.test(fontcolor)) fontcolor = '#000000';
+    else fontcolor = `#${fontcolor}`
+
+    if (!font || !GlobalFonts.has(font)) font = "arial"
+    font = font.toLowerCase();
+
+    if (!backgroundcolor || !hexColorRegex.test(backgroundcolor)) backgroundcolor = colors[Math.round(Math.abs(text.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)) % 100) % colors.length];
+    else backgroundcolor = `#${backgroundcolor}`
+
+    const canvas = createCanvas(size, size);
+    const ctx = canvas.getContext('2d');
+
     // Create image with letter a and backround random baed in the number of the string converted to number
-    ctx.fillStyle = colors[Math.round(Math.abs(text.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)) % 100) % colors.length];
+    ctx.fillStyle = backgroundcolor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-    ctx.font = '48px arial';
+    ctx.fillStyle = fontcolor;
+    ctx.font = `${fontsize}px ${font}, arial`;
     ctx.fillText(text, canvas.width / 2 - ctx.measureText(text).width / 2, canvas.height / 2 + 12);
-// Save image to cache
-    await cache.set(text, await canvas.encode('png'));
+    // Save image to cache
+    await cache.set(`${text} ${fontsize} ${size} ${fontcolor} ${backgroundcolor} ${font}`, await canvas.encode('png'));
     res.setHeader('Content-Type', 'image/png');
     res.send(await canvas.encode('png'));
-    
+
 });
 
 module.exports = router;
