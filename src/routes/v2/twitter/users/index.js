613c9d927bd75cac57d3c75750b072e05ab9a009
José Ortiz <me@mdcdev.me>
2024-08-06 22:19:15 +0000

    refactor: Update Twitter API response for likes endpoint and add avatar decorator

--- src/routes/v2/twitter/users/index.js
+++ src/routes/v2/twitter/users/index.js
@@ -12,9 +12,9 @@ const { responseHandler } = require(join(__basedir, 'utils', 'utils'));
 const userCache = new Cache("twitter-users", 0, 60 * 60 * 24 * 1.5)
 const tweetsCache = new Cache("twitter-users-tweets", 0, 60 * 60 * 6)
 const repliesCache = new Cache("twitter-users-replies", 0, 60 * 60 * 6)
-const likesCache = new Cache("twitter-users-likes", 0, 60 * 60 * 6)
 const followersCache = new Cache("twitter-users-followers", 0, 60 * 60 * 6)
 const followingsCache = new Cache("twitter-users-followings", 0, 60 * 60 * 6)
+const highlightsCache = new Cache("twitter-users-highlights", 0, 60 * 60 * 6)
 
 const rettiwt = new Rettiwt({ apiKey: process.env.TWITTER_TOKEN });
 
@@ -68,7 +68,7 @@ router.get('/:username', limit, async (req, res) => {
 
 });
 
-router.get('/:username/avatar', limit, async (req, res) => {
+router.get('/:username/avatar(:ext)?', limit, async (req, res) => {
     let { username } = req.params;
     username = username.toLowerCase()
     let data = await userCache.get(username);
@@ -93,7 +93,7 @@ router.get('/:username/avatar', limit, async (req, res) => {
 
 })
 
-router.get('/:username/banner', limit, async (req, res) => {
+router.get('/:username/banner(:ext)?', limit, async (req, res) => {
     let { username } = req.params;
     username = username.toLowerCase()
     let data = await userCache.get(username);
@@ -220,43 +220,9 @@ router.get('/:username/replies', limit, async (req, res) => {
 
 })
 
+// Deprecated: Twitter (API) doesn't allow to get the likes of a tweet, for now we will return an empty array
 router.get('/:username/likes', limit, async (req, res) => {
-    let { username } = req.params;
-    username = username.toLowerCase()
-    let data = await userCache.get(username);
-    if (!data) {
-        await rettiwt.user.details(username).then(async details => {
-            //If the response is 200, add the user to the cache
-            if (details) {
-                await userCache.set(username, details);
-                data = details;
-            } else {
-                return statusCodeHandler({ statusCode: 404 }, res);
-            }
-        }).catch((e) => {
-            return statusCodeHandler({ statusCode: 15001 }, res);
-        })
-    }
-
-    if (!data?.id) return;
-
-    if (await likesCache.has(username)) return responseHandler(req.headers.accept, res, { tweets: (await likesCache.get(username)).list }, 'tweets');
-
-    await rettiwt.user.likes(data.id, 100).then(async details => {
-        if (details) {
-            await likesCache.set(username, details);
-            return responseHandler(req.headers.accept, res, { tweets: details.list }, 'tweets');
-        } else {
-            return statusCodeHandler({ statusCode: 15003 }, res);
-        }
-
-    }).catch((e) => {
-        console.log(e)
-        return statusCodeHandler({ statusCode: 15003 }, res);
-    })
-
-    return;
-
+    return responseHandler(req.headers.accept, res, { tweets: [], message: "Twitter (API) doesn't allow to get the likes of a tweet" }, 'tweets');
 })
 
 router.get('/:username/followers', limit, async (req, res) => {
@@ -358,6 +324,41 @@ router.get('/:username/media', limit, async (req, res) => {
     await rettiwt.user.media(account.id).then(async details => {
         if (details) {
             return responseHandler(req.headers.accept, res, details.list, "media");
+        } else {
+            return statusCodeHandler({ statusCode: 15003 }, res);
+        }
+
+    }).catch((e) => {
+        console.log(e)
+        return statusCodeHandler({ statusCode: 15003 }, res);
+    })
+
+    return;
+
+})
+
+router.get('/:username/highlights', limit, async (req, res) => {
+    let { username } = req.params;
+    username = username.toLowerCase()
+
+    let account = await axios.get(req.protocol + '://' + req.get('host') + '/v2/twitter/users/' + username, {
+        headers: {
+            "x-api-key": process.env.INTERNAL_API_KEY
+        }
+    }).then(res => res.data).catch(e => null);
+
+    if (await highlightsCache.has(username)) return responseHandler(req.headers.accept, res, { tweets: (await highlightsCache.get(username)).list }, 'tweets');
+
+    await rettiwt.user.highlights(account.id).then(async details => {
+        if (details) {
+            await highlightsCache.set(username, details);
+            const tweets = details.list;
+            tweets.forEach(tweet => {
+                if (tweet.tweetBy) {
+                    delete tweet.isVerified;
+                }
+            })
+            return responseHandler(req.headers.accept, res, { tweets }, 'tweets');
         } else {
             return statusCodeHandler({ statusCode: 15003 }, res);
         }
