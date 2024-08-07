86a678b9426a47cc2e36b366ad527dc93a2afad9
José Ortiz <me@mdcdev.me>
2024-08-07 04:30:16 +0000

    refactor: Update error-handler.js to use relative import path for statusCodeHandler

--- src/utils/api/error-handler.js
+++ src/utils/api/error-handler.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-const { statusCodeHandler } = require(`../status-code-handler`);
+const { statusCodeHandler } = require('../status-code-handler');
 
 router.all('*', (req, res, next) => {
     try {
