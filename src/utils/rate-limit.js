07b6de51514f250792a4b5a32228a954545f3d24
Jose Ortiz <me@mdcdev.me>
2024-09-06 21:38:08 -0500

    Refactorizar manejo de errores y recuperación de caché en rutas de tweets y datos del repositorio

--- src/utils/rate-limit.js
+++ src/utils/rate-limit.js
@@ -1,27 +1,46 @@
-const { DB } = require('./db');
+const { DB } = require("./db");
 const client = new DB(1, "rate-limit");
-const RedisStore = require('rate-limit-redis');
-const rateLimit = require('express-rate-limit')
+const RedisStore = require("rate-limit-redis");
+const rateLimit = require("express-rate-limit");
 
-module.exports = (minutes, max) => {
-        return rateLimit({
-            windowMs: 1000 * 60 * minutes, // 15 minutes
-            max: (req, res) => {
-                return max;
-            },
-            message: (req, res) => {
-                statusCodeHandler({ statusCode: 10001 }, res);
-            },
-            skip: (req) => {
-                if (req.headers['x-api-key'] === process.env.INTERNAL_API_KEY) return true;
-                if (req.headers['X-RapidAPI-Proxy-Secret'] === process.env.RAPIDAPI_PROXY_SECRET) return true;
-        
-                return false;
-            },
-            standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-            legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-            store: new RedisStore({
-                sendCommand: (...args) => client.sendCommand(args),
-            })
-        })
-}
+module.exports = (minutes = 15, max = 50) => {
+  return rateLimit({
+    windowMs: 1000 * 60 * minutes, // 15 minutes
+    max: (req, res) => {
+      return max;
+    },
+    message: (req, res) => {
+      statusCodeHandler({ statusCode: 10001 }, res);
+    },
+    skip: (req) => {
+      const [version, app] = req.originalUrl.split("/");
+
+      if (req.headers["x-api-key"] === process.env.INTERNAL_API_KEY)
+        return true;
+      if (
+        req.headers["x-rapidapi-proxy-secret"] ===
+        process.env.RAPIDAPI_PROXY_SECRET
+      )
+        return true;
+
+      if (version === "v1") return false;
+
+      if (version === "v2") {
+        switch (app) {
+          case "discord":
+            if (req.headers["discord-bot-token"]) return true;
+            break;
+          default:
+            return false;
+        }
+      }
+
+      return false;
+    },
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    store: new RedisStore({
+      sendCommand: (...args) => client.sendCommand(args),
+    }),
+  });
+};
